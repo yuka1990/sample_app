e4bad711ef1fd39ed3826da681488ac179ffa8fb
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-04-04 00:04:18 +0000

    [update]5章まで

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,7 +5,7 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -13,10 +13,19 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list= List.find(params[:id])
   end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
   private
    def list_params
      params.require(:list).permit(:title, :body)
